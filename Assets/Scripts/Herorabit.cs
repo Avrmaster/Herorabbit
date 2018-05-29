6bdbe619297f237838973ec9de3a997be41ba931
Oleksandr Leskiv <sasha26.05.2012@gmail.com>
2018-05-29 08:17:42 +0300

    Initial commit

---
+++ Assets/Scripts/Herorabit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class Herorabit : MonoBehaviour
+{
+
+	public float Speed = 2;
+	private Rigidbody2D _bunny;
+	
+	// Use this for initialization
+	private void Start ()
+	{
+		_bunny = GetComponent<Rigidbody2D>();
+	}
+	
+	private void FixedUpdate()
+	{
+		var value = Input.GetAxis("Horizontal");
+		if ((Mathf.Abs(value) > 0))
+		{
+			var velocity = _bunny.velocity;
+			velocity.x = value * Speed;
+			_bunny.velocity = velocity;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			var velocity = _bunny.velocity;
+			velocity.y = 2;
+			_bunny.velocity = velocity;
+		}
+		
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			_bunny.MoveRotation(_bunny.rotation+10);
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			_bunny.MoveRotation(_bunny.rotation-10);
+		}
+		
+	}
+}
